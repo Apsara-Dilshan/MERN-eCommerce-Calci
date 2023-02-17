116d6765bc87de22baaff5aefbf9122a83e51147
Apsara-Dilshan <apsaradilshan99@gmail.com>
2023-02-16 19:36:40 -0800

    fetch products from backend

---
+++ public/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Grey Tshirt',
+      slug: 'Grey-Tshirt',
+      category: 'TShirts',
+      image:
+        'https://images.squarespace-cdn.com/content/v1/5c3f51e325bf02ae29a22fad/1574454685200-IF9ZFN8XO5JR3HXC3PAG/Karis-Kids-tshirt-heather-dark-grey-reverse.jpg?format=1500w', // 679px × 829px
+      price: 120,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Long Sleeve Denim Shirt',
+      slug: 'long-sleeve-denim-shirt',
+      category: 'Shirts',
+      image:
+        'https://media.istockphoto.com/id/584479824/photo/blue-jean-shirt-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=_yZ_NkKcn_P-eFVlqtB2RsAx8A9sFEQIjSU_xTwIFdY=',
+      price: 250,
+      countInStock: 4,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Red Hoodie',
+      slug: 'red-hoodie',
+      category: 'Jackets',
+      image:
+        'http://images.squarespace-cdn.com/content/v1/5a8d4b3929f187a729abbe4d/1627576661210-4J6KRG10ZEPKNZ2LGMJA/unisex-heavy-blend-hoodie-red-front-6102d94cf31d5.jpg',
+      price: 150,
+      countInStock: 7,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image:
+        'https://cdn.shopify.com/s/files/1/0692/4255/products/Black_TuxPants_800x.jpg?v=1646407013',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
