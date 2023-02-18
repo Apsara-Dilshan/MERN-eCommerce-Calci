892db389e8574a12100ff3e7904a56b0420df82b
Apsara-Dilshan <apsaradilshan99@gmail.com>
2023-02-17 20:07:01 -0800

    Signin done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -32,6 +32,7 @@ function HomeScreen() {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
         const result = await axios.get('/api/products');
+
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
